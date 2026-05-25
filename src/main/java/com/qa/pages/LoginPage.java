1411c3a1dd450654fde701a9c09ca3caabcd39f4
Srinivas <skasetti@gmail.com>
2026-05-25 18:05:24 -0400

    added serach page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,14 @@
 package com.qa.pages;
 
 public class LoginPage {
+	
+	public void loginPage(){
+		System.out.println("login page");
+		
+	}
+	
+	public void loginSet(){
+		System.out.println("login set");
+	}
 
 }
